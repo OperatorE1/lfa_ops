5fec6972d63b71345a1d0a7317ab3397b1a97c8b
OperatorE <operatoreme@gmail.com>
2022-05-22 11:15:22 +0000

    brute force 2

--- crosslinks/solve.py
+++ crosslinks/solve.py
@@ -2,25 +2,31 @@ import sys
 from telnetlib import Telnet
 
 
-def force(satNum):
-    with Telnet( "crosslinks.satellitesabove.me", 5300 ) as tn:
-        out = tn.read_until(b"please:\n")
-        #print (out)
-        tn.write(b"ticket{bravo684427romeo3:GEDCwFDqDnvyG2MYS2zQ5mN8UOuJtFo-NiWDKikuX17Clx5I2TULa2-QqG6i8hjTjw}\n")
-
-        out = tn.read_until(b"am I:")
-
-        #print (out)
-
-        tn.write(satNum.encode('ascii') + b"\n")
-
-        out = tn.read_all()
-        print (out.decode('ascii'))
 
 
 if __name__ == "__main__":
-    for n in range(101,180):
-        print(n)
 
-        force("%s"%n)
+    for n in range(101,180):
+        with Telnet( "crosslinks.satellitesabove.me", 5300 ) as tn:
+            out = tn.read_until(b"please:\n")
+            #print (out)
+            tn.write(b"ticket{bravo684427romeo3:GEDCwFDqDnvyG2MYS2zQ5mN8UOuJtFo-NiWDKikuX17Clx5I2TULa2-QqG6i8hjTjw}\n")
+
+            
+            out = tn.read_until(b"am I:")
+
+            #observation 1
+            satNum1 = "SATELLITE 155"
+            print ("observation 1 - %s" % satNum1)
+            tn.write(satNum1.encode('ascii') + b"\n")
+
+
+            out = tn.read_until(b"am I:")
+            satNum = "%s"%n
+            tn.write(satNum.encode('ascii') + b"\n")
+
+            #print (out) 
+
+            out = tn.read_all()
+            print (out.decode('ascii'))
 
